7f305db565ed51c1f4974675d3ff7aaaa8591a1f
Patito <patriciabaez1987@gmail.com>
2023-04-24 00:11:19 -0300

    correccion de codigo url

--- api_peliculas/Controllers/MoviesController.cs
+++ api_peliculas/Controllers/MoviesController.cs
@@ -9,48 +9,39 @@ namespace api_peliculas.Controllers
     public class MoviesController : Controller
     {
         private readonly HttpClient client = new HttpClient();
-        JsonSerializerOptions options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true }; 
+        JsonSerializerOptions options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
 
         [HttpGet("{title}", Name = "GetMovies")]
         public async Task<ActionResult<MovieModel>> GetMovies(string title)
         {
-            HttpResponseMessage response = await client.GetAsync("https://api.themoviedb.org/3/movie/now_playing?api_key=a847973815573f73a0ba6c59d790d0f9&language=es-ES&page=1");
+            HttpResponseMessage response = await client.GetAsync("https://api.themoviedb.org/3/search/movie?api_key=a847973815573f73a0ba6c59d790d0f9&query=" + title);
             response.EnsureSuccessStatusCode();
             string responseBody = await response.Content.ReadAsStringAsync();
             var dataResult = JsonSerializer.Deserialize<MovieCompleteModel>(responseBody, options);
 
             MovieModel movieList = new MovieModel();
             movieList.TitleYear = new List<string>();
-            
-            for (int i = 0; i < dataResult.Results.Count - 1; i++)
+
+            movieList.Title = dataResult.Results[0].Title;
+            movieList.Original_Title = dataResult.Results[0].original_title;
+            movieList.Vote_Average = dataResult.Results[0].vote_average;
+            movieList.Release_Date = dataResult.Results[0].release_date;
+            movieList.Overview = dataResult.Results[0].Overview;
+
+            for (int i = 1; i < dataResult.Results.Count - 1; i++)
             {
-                if (title.Trim().ToUpper() == dataResult.Results[i].Title.Trim().ToUpper())
+                if (movieList.TitleYear.Count < 5)
                 {
-                    movieList.Title = dataResult.Results[i].Title;
-                    movieList.Original_Title = dataResult.Results[i].Original_Title;
-                    movieList.Vote_Average = dataResult.Results[i].Vote_Average;
-                    movieList.Release_Date = dataResult.Results[i].Release_Date.ToString("dd/MM/yyyy");
-                    movieList.Overview = dataResult.Results[i].Overview;
-                    var genero = dataResult.Results[i].Genre_Ids;
-                    for (int j = 0; j < dataResult.Results.Count - 1; j++)
-                    {
-                        foreach (var k in dataResult.Results[j].Genre_Ids)
-                        {
-                            if (genero[0] == k && title.Trim().ToUpper() != dataResult.Results[j].Title.Trim().ToUpper())
-                            {
-                                if (movieList.TitleYear.Count < 5)
-                                {
-                                    movieList.TitleYear.Add(dataResult.Results[j].Title + " (" + (dataResult.Results[j].Release_Date).ToString("yyyy") + ")");
-                                }
-                                break;
-                            }
-                        }
-                    }
+                    movieList.TitleYear.Add(dataResult.Results[i].Title + " (" + dataResult.Results[i].release_date + ")");
+                }
+                else
+                {
+                    break;
                 }
             }
             if (movieList.Title == null)
             {
-                return NotFound("No existe el titulo");
+                return NotFound("Titulo no encontrado");
             }
             return movieList;
         }
--- api_peliculas/Models/MovieCompleteModel.cs
+++ api_peliculas/Models/MovieCompleteModel.cs
@@ -2,37 +2,30 @@
 {
     public partial class MovieCompleteModel
     {
-        public Dates Dates { get; set; }
         public long Page { get; set; }
         public List<Result> Results { get; set; }
-        public long Total_Pages { get; set; }
-        public long Total_Results { get; set; }
-    }
-
-    public partial class Dates
-    {
-        public DateTimeOffset Maximum { get; set; }
-        public DateTimeOffset Minimum { get; set; }
+        public long TotalPages { get; set; }
+        public long TotalResults { get; set; }
     }
 
     public partial class Result
     {
         public bool Adult { get; set; }
-        public string Backdrop_Path { get; set; }
-        public List<long> Genre_Ids { get; set; }
+        public string backdrop_path { get; set; }
+        public List<long> genre_ids { get; set; }
         public long Id { get; set; }
         public OriginalLanguage OriginalLanguage { get; set; }
-        public string Original_Title { get; set; }
+        public string original_title { get; set; }
         public string Overview { get; set; }
         public double Popularity { get; set; }
-        public string Poster_Path { get; set; }
-        public DateTime Release_Date { get; set; }
+        public string poster_path { get; set; }
+        public string release_date { get; set; }
         public string Title { get; set; }
         public bool Video { get; set; }
-        public double Vote_Average { get; set; }
-        public long Vote_Count { get; set; }
+        public double vote_average { get; set; }
+        public long vote_count { get; set; }
     }
 
-    public enum OriginalLanguage { En, Es };
+    public enum OriginalLanguage { En, Ja, No, Te };
 
 }
